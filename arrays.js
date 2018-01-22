460fdea8de03e7acd708f1a0eb74690bbca36225
Sharon Clapp <sbclapp@gmail.com>
2018-01-22 16:37:12 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,5 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(arrayName, element) {
+     return arrayName = [element, ...arrayName];
+}
